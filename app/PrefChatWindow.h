682df4bd4e1e878cddd334b4f97e128d7e8b4933
Rei <chloerei@gmail.com>
2010-05-15 03:49:19 +0000

    Add License info: GPLv3

--- app/PrefChatWindow.h
+++ app/PrefChatWindow.h
@@ -1,3 +1,29 @@
+/*
+ * Copyright (C) 2010 Rei
+ *
+ * Author:
+ *	Rei
+ *
+ * Source:
+ *	http://github.com/chloerei/qtalk
+ *
+ * This file is a part of QTalk.
+ *
+ * QTalk is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * QTalk is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with QTalk.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
 #ifndef PREFCHATWINDOW_H
 #define PREFCHATWINDOW_H
 
